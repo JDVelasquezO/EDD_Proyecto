b95e2b8da0dd3a6937dcc4103e8da1d70ec852b1
José Daniel Velásquez Orozco <jdvela158@gmail.com>
2020-12-22 23:05:30 -0600

    Update Node.py

--- Node.py
+++ Node.py
@@ -1,30 +1,30 @@
 class Node:
-
+    
     def __init__(self):
         self.array = []
-        self.key = 0
+        self.key = -1
         self.pk = None
+        self.isGeneric = False
 
-    def insert(self, dato):
-        self.array.append(dato)
-        lista = self.array.copy()
+    def insert(self, dato, key):
+        self.array.append((key,dato)) #ahora recibe el parametro key 
+        lista = self.array[:]
         lista_ordenada= self.quick_sorted(lista)
-        self.array.clear()
-        for i in lista_ordenada:
-            self.array.append(i)
+        self.array = lista_ordenada[:]
 
     def buscarDato_binary(self, dato):
-        ub = len(self.array) 
-        lb = 0 
-        while lb <= ub-1:
-            med = (ub + lb ) // 2
-            arreglo = self.array[med]
-            if int(arreglo[0]) == int(dato[0]):
+        inicio = 0
+        final = len(self.array) -1 
+        while inicio <= final:
+            mid = inicio + (final - inicio) //2
+            arreglo = self.array[mid]
+            # if int(arreglo[0]) == int(dato):
+            if int(arreglo[0]) == int(dato):
                 return True
-            elif int(arreglo[0]) < int(dato[0]):
-                lb = med +1
-            elif int(arreglo[0]) > int(dato[0]):
-                ub = med -1
+            elif int(dato) < int(arreglo[0]):
+                final = mid -1 
+            else:
+                inicio = mid +1
         return False
 
     def busquedaB(self, dato):
@@ -33,13 +33,14 @@ class Node:
         while inicio <= final:
             mid = inicio + (final - inicio) //2
             arreglo = self.array[mid]
+            # if int(arreglo[0]) == int(dato):
             if int(arreglo[0]) == int(dato):
                 return arreglo
             elif int(dato) < int(arreglo[0]):
                 final = mid -1 
             else:
                 inicio = mid +1
-        return None
+        return []
 
     def quick_sorted(self, sequencia):
         lista = sequencia
@@ -78,8 +79,9 @@ class Node:
             while inicio <= final:
                 mid = inicio + (final - inicio) //2
                 arreglo = self.array[mid]
+                # if int(arreglo[0]) == int(key):
                 if int(arreglo[0]) == int(key):
-                    self.array[mid][columna] = modificacion
+                    self.array[mid][1][columna] = modificacion
                     return 0
                 elif int(key) < int(arreglo[0]):
                     final = mid -1 
@@ -95,6 +97,7 @@ class Node:
         while inicio <= final:
             mid = inicio + (final - inicio) //2
             arreglo = self.array[mid]
+            # if int(arreglo[0]) == int(dato):
             if int(arreglo[0]) == int(dato):
                 self.array.pop(mid)
                 return True
@@ -103,3 +106,81 @@ class Node:
             else:
                 inicio = mid +1
         return None
+
+    def obtenerLower(self,columna,valor,lower): ##Cristian 17/12/2020
+        let=""
+        contador = 0
+        x= len(lower)-1
+        for i in valor[columna]:
+            if contador > x:
+                if lower.upper() == let.upper():
+                    return True
+                else:
+                    return False
+            else:
+                contador+=1
+                let+=i
+
+    def obtenerUpper(self,columna,valor,upper):
+        let=""
+        contador = 0
+        x= len(upper)-1
+        valor1 = valor[columna]
+        for i in valor1[::-1]:
+            if contador > x:
+                if upper.upper() == let[::-1].upper():
+                    return True
+                else:
+                    return False
+            else:
+                contador+=1
+                let+=i    
+
+    def Numbervalidation(self,columnNumber,valor,lower,upper):       
+        if int(valor[columnNumber]) <= upper and int(valor[columnNumber]) >= lower :
+                return valor
+        else:
+            return None
+
+    def NumbervalidationFloat(self,columnNumber,valor,lower,upper):       
+        if float(valor[columnNumber]) <= upper and float(valor[columnNumber]) >= lower :
+                return valor
+        else:
+            return None        
+
+    def imp_column(self,subnodo,columnNumber,lower,upper): 
+        if isinstance(lower, int) == True:       ##para enteros
+            return self.Numbervalidation(columnNumber,subnodo,lower,upper)  
+        elif isinstance(lower, float) == True:       ##para float
+            return self.NumbervalidationFloat(columnNumber,subnodo,lower,upper)  
+        else:                                       ##para string
+            if self.obtenerLower(columnNumber,subnodo,lower) == True and self.obtenerUpper(columnNumber,subnodo,upper) == True: ##
+                return subnodo
+            else:
+                return None
+
+    def imp_column2(self,columnNumber,lower,upper): 
+        if isinstance(lower, int) == True:
+            for i in self.array:     
+                return self.Numbervalidation(columnNumber,i[1],lower,upper)      
+        elif isinstance(lower, float) == True:       ##para float
+            for i in self.array:     
+                return self.NumbervalidationFloat(columnNumber,i[1],lower,upper)          
+        else:
+            for i in self.array:
+                if self.obtenerLower(columnNumber,i[1],lower) == True and self.obtenerUpper(columnNumber,i[1],upper) == True:
+                    return i[1]
+                else:
+                    return None
+
+    #agrega una columna y registra un dato
+    def alterAddColumn(self, dato):
+        try:
+            for i in self.array:
+                i[1].append(dato)
+            # print("ya jalo")
+        except Exception as e:
+            print("########")
+            print("en el nodo")
+            print(e)
+            print("########")
